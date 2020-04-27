1324210f4fbdc07e41a10caee38dde26043fb43d
yicheny <1463020554@qq.com>
2020-04-27 11:02:34 +0800

    update 项目 表格宽度调整--#9-1

--- demo/src/view/TableView.js
+++ demo/src/view/TableView.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import _ from 'lodash';
 import {Card,Table} from '../lib'
 import {N2} from '../utils/format';
 import {data} from '../data/tableData';
 
-const dataSource = data.slice(0,40);
+const dataSource = _.orderBy(data.slice(0,40),'strategy');
 const columns = [
-    {name:'#',render:(v,o,i)=>i+1,width:40,align:'center',lock:true},
+    // {name:'#',render:(v,o,i)=>i+1,width:40,align:'center',lock:true},
+    {name:'投资策略',code:'strategy',width:100,align:'left',lock:true},
     {name:'名称',code:'productName',width:280,align:'left',lock:true},
     {name:'管理机构',code:'fundAdmin',width:160,align:'left'},
     {name:'基金经理',code:'manager',width:160,align:'left'},
-    {name:'投资策略',code:'strategy',width:100,align:'left'},
     {name:'成立年限(年)',code:'duration',width:100,align:'right'},
     {name:'最新净值',code:'unitNetValue',render:N2,width:100,align:'right'},
     {name:'自成立收益率',code:'pnlPctAll',render:N2,width:100,align:'right'},
